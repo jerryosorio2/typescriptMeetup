02f6c69c7e79f304cbcf7990fb6c0573da8f3006
Jerry Osorio <jerry.osorio2@gmail.com>
2021-05-15 22:35:23 -0600

    adding some exatra examples

---
+++ src/2-EcmascriptExample/Calculator.ts
@@ -0,0 +1,20 @@
+let number1: number = 10;
+let number2: number = 2;
+
+function add(n1: number, n2: number): number {
+  return n1 + n2;
+}
+
+function substract(n1: number, n2: number): number {
+  return n1 - n2;
+}
+
+function multiply(n1: number, n2: number): number {
+  return n1 * n2;
+}
+
+function divide(n1: number, n2: number): number {
+  return n1 / n2;
+}
+
+console.log(`add result: ${add(number1, number2)}`);
---
+++ src/2-EcmascriptExample/FeaturesExamples.ts
@@ -0,0 +1,13 @@
+let userName:string = "Jerry";
+let age:number = 31;
+let isActive:boolean = true;
+let hobbies: string[] = ["baseball","football","read"];
+
+
+
+let message:string;
+
+
+message = `username: ${userName} with age: ${age} status: ${isActive}`;
+console.log(message);
+console.log(`hobbies are: ${hobbies}`);
